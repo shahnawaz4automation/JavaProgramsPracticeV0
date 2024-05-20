99a0a9eb79dc94d75d0c005859544ff93fd9fc00
mohammed shahnawaz <shah4corp@gmail.com>
2024-05-20 21:10:26 +0530

    20052024 fourth commit

--- src/java_programs_practice_2052024/PrintName.java
+++ src/java_programs_practice_2052024/PrintName.java
@@ -2,6 +2,6 @@ package java_programs_practice_2052024;
 
 public class PrintName {
 	public static void main(String[] args) {
-		System.out.println("First java program edited in github");
+		System.out.println("First java program edited in github, edited again in local");
 	}
 }
