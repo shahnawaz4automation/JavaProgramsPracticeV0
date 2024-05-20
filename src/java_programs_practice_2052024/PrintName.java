81ae79ddbbacd7050f7496ee5e550408c37449ca
Mohammed Shahnawaz <48588547+shahnawaz4automation@users.noreply.github.com>
2024-05-20 21:37:33 +0530

    20052024 seventh commit from github

--- src/java_programs_practice_2052024/PrintName.java
+++ src/java_programs_practice_2052024/PrintName.java
@@ -3,6 +3,6 @@ package java_programs_practice_2052024;
 public class PrintName {
 	public static void main(String[] args) {
 		System.out.println("First java program edited in github, edited again in local");
-		System.out.println("Added new syso, updated");
+		System.out.println("Second output");
 	}
 }
