5758535f650b92e7feeb42b9fa75b834ac0c44e3
mohammed shahnawaz <shah4corp@gmail.com>
2024-05-23 21:49:41 +0530

    23052024 first commit

---
+++ src/java_programs_practice_2052024/Swap2Numbers1stway.java
@@ -0,0 +1,13 @@
+package java_programs_practice_2052024;
+
+public class Swap2Numbers1stway {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		c = a;
+		a = b;
+		b =c;
+		System.out.println(a+" "+b);
+	}
+}
