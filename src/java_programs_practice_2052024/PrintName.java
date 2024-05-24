6f34c916fb2712b1b3736f68b260ddda2053b866
mohammed shahnawaz <shah4corp@gmail.com>
2024-05-24 21:12:03 +0530

    24052024 second commit

--- src/java_programs_practice_2052024/PrintName.java
+++ src/java_programs_practice_2052024/PrintName.java
@@ -6,5 +6,6 @@ public class PrintName {
 		System.out.println("Second output");
 		System.out.println("Third output");
 		System.out.println("Fourth output");
+		System.out.println("Fifth output");
 	}
 }
