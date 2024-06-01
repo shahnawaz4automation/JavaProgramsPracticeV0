c903e46be8a8d6c99ad926cf741fef3205a18520
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-01 23:57:49 +0530

    01062024 third commit

--- src/java_programs_practice_2052024/StringMethods.java
+++ src/java_programs_practice_2052024/StringMethods.java
@@ -3,7 +3,9 @@ package java_programs_practice_2052024;
 public class StringMethods {
 	public static void main(String[] args) {
 		String s = "welcome";
+		String s1 = "to java";
 		
 		System.out.println(s.length());
+		System.out.println(s.concat(s1));
 	}
 }
