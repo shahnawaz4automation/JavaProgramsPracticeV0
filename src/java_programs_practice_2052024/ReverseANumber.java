b556bc7db5fc1a4a70a33a38bd427751e46dc94f
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-01 23:55:03 +0530

    01062024 first commit

---
+++ src/java_programs_practice_2052024/ReverseANumber.java
@@ -0,0 +1,14 @@
+package java_programs_practice_2052024;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		//1. Using StringBuffer class
+	}
+
+}
---
+++ src/java_programs_practice_2052024/StringMethods.java
@@ -0,0 +1,7 @@
+package java_programs_practice_2052024;
+
+public class StringMethods {
+	public static void main(String[] args) {
+		String s = "welcome";
+	}
+}
