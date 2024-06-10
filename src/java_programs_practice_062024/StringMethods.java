d095bc3b44806e42ca291225575ef3e6a5efcd79
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-10 21:07:10 +0530

    21:06 10062024 commit

--- src/java_programs_practice_062024/StringMethods.java
+++ src/java_programs_practice_062024/StringMethods.java
@@ -1,7 +1,49 @@
 package java_programs_practice_062024;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class StringMethods {
 	public static void main(String[] args) {
+		String s = "welcome";
+		s.length();
+		System.out.println(s.length());
+		System.out.println("shahnawaz".length());
+	
+		String s1 = "welcome to";
+		String s2 = "selenium";
+		String s3 = "automation";
+		System.out.println(s1+s2);
+		System.out.println(s1+s2+s3);
+		System.out.println(s1.concat(s2));
 		
+		String a = "abc";
+		String b = new String("abc");
+		
+		System.out.println(a==b);
+		System.out.println(a.equals(b));
+		
+		String c = "abc";
+		String d = new String("abc");
+		d=d.intern();
+		
+		System.out.println(c==d);
+		System.out.println(c.equals(d));
+		
+		a.length();
+		a.concat(b);
+		a.trim();
+		a.charAt(2);
+		a.contains(b);
+		a.equals(b);
+		a.equalsIgnoreCase(b);
+		a.replace('a', 'b');
+		a.substring(0,2);
+		a.toUpperCase();
+		a.split(" ");
+		
+		String email = "shah4corp@gmail.com";
+		String e[] = email.split("@");
+		System.out.println(Arrays.toString(e));
 	}
 }
