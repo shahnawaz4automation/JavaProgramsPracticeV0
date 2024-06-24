7e5b664697f1049caee0100131965d4ce8a02520
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-06-24 22:27:29 +0530

    73838222724062024 commit

---
+++ src/java_programs_practice_062024/SumOfArray.java
@@ -0,0 +1,24 @@
+package java_programs_practice_062024;
+
+public class SumOfArray {
+	static int sum = 0;
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4};
+		
+		for (int i = 0; i < a.length; i++) {
+			int num = 10;
+			sum = sum + a[i];
+			System.out.println(num);
+		}
+		System.out.println(sum);
+		//System.out.println(num);
+	}
+	
+	public void sum(int a) {
+		System.out.println(a);
+	}
+	
+	public void sum(double b) {
+		System.out.println(b);
+	}
+}
