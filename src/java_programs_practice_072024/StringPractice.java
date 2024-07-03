f5ccf2e81f9a5a157eb6f6c9b3e3f3993979dc9e
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-03 20:47:11 +0530

    823412047030724 commit

---
+++ src/java_programs_practice_072024/StringPractice.java
@@ -0,0 +1,15 @@
+package java_programs_practice_072024;
+
+public class StringPractice {
+
+	public static void main(String[] args) {
+		//String s = "welcome";
+		//String s1 = new String("welcome");
+		//System.out.println(s+" "+s1);
+		
+		String s = "welcome";
+		System.out.println(s.length());
+		System.out.println("Kulsum".length());
+	}
+
+}
