9ae208a57e36c9fa95f13ab9b99d2d12cddeca76
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-03 21:47:02 +0530

    54233030724 commit

--- src/java_programs_practice_072024/StringPractice2.java
+++ src/java_programs_practice_072024/StringPractice2.java
@@ -3,8 +3,6 @@ package java_programs_practice_072024;
 public class StringPractice2 {
 	public static void main(String[] args) {
 		String s = "Welcome to selenium practice again and for final time";
-		s=s.replace('m', 'M');
-		System.out.println(s);
-		System.out.println(s.replace('a', 'A'));
+		System.out.println();
 	}
 }
