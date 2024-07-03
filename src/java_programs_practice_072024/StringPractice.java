626f762752aafcfd7459a1467da44e130e2ad376
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-03 21:46:30 +0530

    542332030724 commit

--- src/java_programs_practice_072024/StringPractice.java
+++ src/java_programs_practice_072024/StringPractice.java
@@ -19,9 +19,27 @@ public class StringPractice {
 		System.out.println(s1.concat(s2));
 		System.out.println(s1.concat(s2).concat(s3));
 		
-		String s4 = " Trimmed ";
+		String s4 = "Trimmed ";
 		System.out.println(s4.trim());
 		System.out.println(s4.charAt(2));
+		System.out.println(s4.indexOf('m'));
+		
+		System.out.println(s4.equalsIgnoreCase("trimmed"));
+		
+		String a = "a1";
+		String b = "a1";
+		String e = "a1";
+		String c = new String("a1");
+		String d = new String("a2");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(a==b);
+		System.out.println(a==e);
+		System.out.println(a==c);
+		System.out.println(c==d);
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+		System.out.println(c.equals(d));
+		
 	}
 
 }
---
+++ src/java_programs_practice_072024/StringPractice2.java
@@ -0,0 +1,10 @@
+package java_programs_practice_072024;
+
+public class StringPractice2 {
+	public static void main(String[] args) {
+		String s = "Welcome to selenium practice again and for final time";
+		s=s.replace('m', 'M');
+		System.out.println(s);
+		System.out.println(s.replace('a', 'A'));
+	}
+}
