57187d4073465a881f848a2e9cfe91b37ff6eb30
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-07 04:12:06 +0530

    23456041124 commit

---
+++ src/java_programs_practice_072024/FactorialOfaNumber.java
@@ -0,0 +1,14 @@
+package java_programs_practice_072024;
+
+public class FactorialOfaNumber {
+
+	public static void main(String[] args) {
+		int num = 4;
+		int factorial = 1;
+		for (int i= 1;i<=num;i++) {
+			factorial = factorial * i;
+		}
+		System.out.println(factorial);
+	}
+
+}
