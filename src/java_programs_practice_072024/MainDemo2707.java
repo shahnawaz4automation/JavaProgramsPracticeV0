d6d2b400c37323370b779cbac0089cb24915e82b
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-07-28 21:39:53 +0530

    438242139280724 commit

---
+++ src/java_programs_practice_072024/MainDemo2707.java
@@ -0,0 +1,14 @@
+package java_programs_practice_072024;
+
+public class MainDemo2707 {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		if (b) {
+			System.out.println("Hello!");
+			return;
+		}
+		System.out.println("Bye");
+	}
+	
+}
