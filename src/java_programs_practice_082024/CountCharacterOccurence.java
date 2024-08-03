490478fd4461f7e7c0aeacaf6576296e0da44e7a
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-08-03 19:27:33 +0530

    32231927030824 commit

---
+++ src/java_programs_practice_082024/CountCharacterOccurence.java
@@ -0,0 +1,17 @@
+package java_programs_practice_082024;
+
+public class CountCharacterOccurence {
+
+	public static void main(String[] args) {
+		String s = "Java is a programming language";
+		char[] a = s.toCharArray();
+		int count = 0;
+		for(int i = 0; i <a.length; i++) {
+			if(a[i]=='g') {
+				count++;
+			}
+		}
+		System.out.println("Number of occurences of a is: "+count );
+	}
+
+}
---
+++ src/java_programs_practice_082024/CountWordsOccurence.java
@@ -0,0 +1,7 @@
+package java_programs_practice_082024;
+
+public class CountWordsOccurence {
+	public static void main(String[] args) {
+
+	}
+}
