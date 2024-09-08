4ab86674fdd850676992013354b47c3c1c3b5305
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-09-08 18:54:54 +0530

    A03ReverseString commit

--- src/java_programs_practice_092024/A02ReverseANumber.java
+++ src/java_programs_practice_092024/A02ReverseANumber.java
@@ -1,5 +1,11 @@
 package java_programs_practice_092024;
 
 public class A02ReverseANumber {
-
+	public static void main(String[] args) {
+		int a = 1234;
+		StringBuffer sb = new StringBuffer(String.valueOf(a));
+		System.out.println(sb.reverse());
+	}
+	
+	
 }
