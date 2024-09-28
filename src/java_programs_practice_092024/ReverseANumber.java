45199f68c23cd2a40b6dc21240e26339aaa46372
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-09-28 15:42:14 +0530

    43242 commit

--- src/java_programs_practice_092024/ReverseANumber.java
+++ src/java_programs_practice_092024/ReverseANumber.java
@@ -1,5 +1,17 @@
 package java_programs_practice_092024;
 
 public class ReverseANumber {
-
+	public static void main(String[] args) {
+		int num = 12345;
+		
+		StringBuffer sb = new StringBuffer(String.valueOf(num));
+		//sb.append();
+		System.out.println(sb.reverse());
+		sb = sb.reverse();
+		int a = Integer.parseInt(sb.toString());
+		System.out.println(a);
+		
+		StringBuilder sb1 = new StringBuilder(String.valueOf(num));
+		System.out.println(sb1.reverse());
+	}
 }
