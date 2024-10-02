b0edad2d61f214305f3c82292d69bef7434a6c84
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-02 08:31:11 +0530

    314313 commit

--- src/java_programs_practice_102024/StaticFinalVariableTest.java
+++ src/java_programs_practice_102024/StaticFinalVariableTest.java
@@ -3,12 +3,12 @@ package java_programs_practice_102024;
 public class StaticFinalVariableTest {
 	//final int a = 10;
 	static int a = 10;
-	public void variable() {
+	public static void variable() {
 		StaticFinalVariableTest.a = 20;
 		System.out.println(a);
 	}
 	public static void main(String[] args) {
-		StaticFinalVariableTest s = new StaticFinalVariableTest();
-		s.variable();
+		//StaticFinalVariableTest s = new StaticFinalVariableTest();
+		StaticFinalVariableTest.variable();
 	}
 }
