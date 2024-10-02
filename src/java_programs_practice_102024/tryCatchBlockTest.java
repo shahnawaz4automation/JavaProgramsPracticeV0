898f5c47716c050527d9bc7d37c684ba7c60e2e6
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-02 07:31:24 +0530

    314313 commit

---
+++ src/java_programs_practice_102024/tryCatchBlockTest.java
@@ -0,0 +1,20 @@
+package java_programs_practice_102024;
+
+import java.io.File;
+
+public class tryCatchBlockTest {
+public static void main(String[] args) {
+	int a = 10;
+	
+	try {
+		System.out.println("1");
+		a=a/0;
+		System.out.println("2");
+	} catch (Exception e) {
+		System.out.println("catch block");
+	}
+	finally {
+		System.out.println("finally block");
+	}
+}
+}
