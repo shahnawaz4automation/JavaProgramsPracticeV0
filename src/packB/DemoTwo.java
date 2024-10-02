c2e9e7da681fb22ba80e3d34ce6e5dcf4416567d
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-02 07:18:41 +0530

    34131 commit

--- src/packB/DemoTwo.java
+++ src/packB/DemoTwo.java
@@ -3,8 +3,12 @@ package packB;
 import packA.ClassAOne;
 import packA.ClassATwo;
 
-public class DemoTwo{
-		
+public class DemoTwo extends ClassAOne {
+	/*public void sample() {
+	   System.out.println(aOneVariable);
+	   aOneMethod();
+	}*/
+
 	public static void main(String[] args) {
 		ClassAOne cAOne = new ClassAOne();
 		System.out.println(cAOne.aOneVariable);
