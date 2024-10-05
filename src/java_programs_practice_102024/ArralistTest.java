76f2092eaf451f2fffcbb4cab3c5770e308b9056
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-05 19:36:22 +0530

    31321 commit

--- src/java_programs_practice_102024/ArralistTest.java
+++ src/java_programs_practice_102024/ArralistTest.java
@@ -1,21 +1,31 @@
 package java_programs_practice_102024;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class ArralistTest {
 	public static void main(String[] args) {
 		ArrayList al = new ArrayList();
-		al.add(100);
+		al.add("A");
 		al.add("welcome");
-		al.add(15.5);
-		al.add('a');
-		al.add(100);
+		al.add("b");
+		al.add("d");
+		al.add("g");
 		
-		Set s = new HashSet();
-		s.addAll(al);
-		System.out.println(s);
+		/*//1. Using for loop
+		for(int i = 0; i<al.size(); i++) {
+			//System.out.println(al.get(i));
+		}
 		
+		//2. Using iterator()
+		Iterator it = al.iterator();
+		while (it.hasNext()) {
+			//System.out.println(it.next());
+		}*/
+		Collections.sort(al);
+		System.out.println(al);
 	}
 }
