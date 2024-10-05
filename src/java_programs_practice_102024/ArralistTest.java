bc1737a7b1689b8e4ebc2a573c8293a564335215
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-05 19:19:25 +0530

    31321 commit

--- src/java_programs_practice_102024/ArralistTest.java
+++ src/java_programs_practice_102024/ArralistTest.java
@@ -1,13 +1,21 @@
 package java_programs_practice_102024;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ArralistTest {
 	public static void main(String[] args) {
-		ArrayList list = new ArrayList();
-		list.add('a');
+		ArrayList al = new ArrayList();
+		al.add(100);
+		al.add("welcome");
+		al.add(15.5);
+		al.add('a');
+		al.add(100);
 		
-		char b = (char) list.get(0);
-		System.out.println(b);
+		Set s = new HashSet();
+		s.addAll(al);
+		System.out.println(s);
+		
 	}
 }
