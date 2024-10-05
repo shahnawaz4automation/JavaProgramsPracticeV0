9d5c974a95f88be7a0084607f627a6e994c5b835
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-10-05 21:57:02 +0530

    LargestOf3Numbers commit

--- src/java_programs_practice_102024/LargestOf3Numbers.java
+++ src/java_programs_practice_102024/LargestOf3Numbers.java
@@ -1,5 +1,13 @@
 package java_programs_practice_102024;
 
 public class LargestOf3Numbers {
-
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = a<b?a:b;
+		int e = d<c?d:c;
+		System.out.println(e);
+	}
 }
