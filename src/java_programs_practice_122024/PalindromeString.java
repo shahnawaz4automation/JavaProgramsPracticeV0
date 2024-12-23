d874cf23102b27992ece030f9d7ea0bd114bc7d1
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-23 19:28:16 +0530

    4132rqwwr commit

--- src/java_programs_practice_122024/PalindromeString.java
+++ src/java_programs_practice_122024/PalindromeString.java
@@ -3,7 +3,7 @@ package java_programs_practice_122024;
 public class PalindromeString {
 
 	public static void main(String[] args) {
-		String str = "ambulance";
+		String str = "sas";
 		String rev = "";
 
 		char[] a = str.toCharArray();
@@ -13,7 +13,7 @@ public class PalindromeString {
 		}
 		System.out.println("Reverse of the str is: " + rev);
 
-		if (str == rev) {
+		if (str.equals(rev)) {
 			System.out.println("String is palindrome");
 		} else {
 			System.out.println("String is not palindrome");
