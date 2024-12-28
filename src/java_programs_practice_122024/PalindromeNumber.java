11a492f64647527edb045b47873c3bb67673f7a4
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-28 14:44:24 +0530

    2812241444 commit

---
+++ src/java_programs_practice_122024/PalindromeNumber.java
@@ -0,0 +1,23 @@
+package java_programs_practice_122024;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		StringBuffer sb = new StringBuffer(String.valueOf(num));
+		//System.out.println(sb.reverse());
+		int rev_num = Integer.parseInt(sb.reverse().toString());
+		
+		if(rev_num == num) {
+			System.out.println("Number is palindrome");
+		}
+		else {
+			System.out.println("Number is not palindrome");
+		}
+		
+	}
+
+}
