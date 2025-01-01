05ad67ee993e218adfb1c9a9f9323d50a1779b07
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-01-01 14:09:44 +0530

    01011409 commit

---
+++ src/java_programs_practice_122024/StringSplitMethod.java
@@ -0,0 +1,14 @@
+package java_programs_practice_122024;
+
+public class StringSplitMethod {
+	public static void main(String[] args) {
+		String text = " Learning java from QAFox.com. ";
+		
+		String[] s = text.split("\\s");
+		for (String a : s) {
+			System.out.println(a);
+		}
+		
+		System.out.println(s[1]);
+	}
+}
