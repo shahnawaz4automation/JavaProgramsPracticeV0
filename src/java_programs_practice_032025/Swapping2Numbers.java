49a6040da900004937509ce22d0e6a7e507246c6
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-03-09 17:10:27 +0530

    345432 commit

---
+++ src/java_programs_practice_032025/Swapping2Numbers.java
@@ -0,0 +1,17 @@
+package java_programs_practice_032025;
+
+public class Swapping2Numbers {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.print(a);
+	}
+
+}
