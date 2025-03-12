6f21c7e8f9c49055c7776e1350dd9435a8438c04
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-03-12 19:42:06 +0530

    8324 commit

---
+++ src/java_programs_practice_032025/ReverseString.java
@@ -0,0 +1,13 @@
+package java_programs_practice_032025;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "ABCD";
+		String rev = "";
+
+		char[] a = str.toCharArray();
+		int len = a.length;
+	}
+
+}
