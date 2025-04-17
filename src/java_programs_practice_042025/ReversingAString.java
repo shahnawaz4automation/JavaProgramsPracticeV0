767a74ffcc133d6402b7ed5364603fafdebc21f1
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-04-17 22:20:29 +0530

    17roqijwf commit

---
+++ src/java_programs_practice_042025/ReversingAString.java
@@ -0,0 +1,13 @@
+package java_programs_practice_042025;
+
+public class ReversingAString {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		c = a;
+		a = b;
+		b = c;
+		System.out.println(a + " " + b);
+	}
+}
