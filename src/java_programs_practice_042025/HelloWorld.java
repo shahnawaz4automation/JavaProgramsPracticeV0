082b2ce4af33581cb879d60bc26b3db93cf203ee
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-04-19 12:16:36 +0530

    09r832jf commit

---
+++ src/java_programs_practice_042025/HelloWorld.java
@@ -0,0 +1,12 @@
+package java_programs_practice_042025;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String name = "Kulsum";
+		int age = 5;
+		System.out.println("Hello, my daughter's name is " + name);
+		System.out.println("She is " + age + " years old.");
+	}
+
+}
