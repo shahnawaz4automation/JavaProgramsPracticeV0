30b499cf2837f7158885447aa116588174430b27
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-21 14:04:32 +0530

    j9asjf commit

--- src/java_programs_practice_032025/newProgram1.java
+++ src/java_programs_practice_032025/newProgram1.java
@@ -2,6 +2,6 @@ package java_programs_practice_032025;
 
 public class newProgram1 {
 public static void main(String[] args) {
-	System.out.println();
+	System.out.println("shahnawaz4automation");
 }
 }
