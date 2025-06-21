f21224e4d8d4a0a37d5dc6c78e57d61def9a06b3
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-21 13:12:29 +0530

    87huisa commit

--- src/java_programs_practice_032025/newProgram.java
+++ src/java_programs_practice_032025/newProgram.java
@@ -2,9 +2,9 @@ package java_programs_practice_032025;
 
 public class newProgram {
 public static void main(String[] args) {
-	int x = 9;
+	/*int x = 9;
 	long y = x;
 	
-	System.out.println(y+10);
+	System.out.println(y+10);*/
 }
 }
