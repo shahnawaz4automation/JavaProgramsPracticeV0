8265afe9af3d6f4a7fc5f713a85464c3055c195d
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-21 13:09:01 +0530

    98ufjas commit

--- src/java_programs_practice_032025/newProgram.java
+++ src/java_programs_practice_032025/newProgram.java
@@ -2,6 +2,9 @@ package java_programs_practice_032025;
 
 public class newProgram {
 public static void main(String[] args) {
-	System.out.println("Starting again inshallah will succeed");
+	int x = 9;
+	long y = x;
+	
+	System.out.println(y+10);
 }
 }
