c750cbfae050a740190e025880f880e9e73ba386
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-26 22:25:33 +0530

    980dsuja commit

--- src/java_programs_practice_0000000062025/ReversingAstring.java
+++ src/java_programs_practice_0000000062025/ReversingAstring.java
@@ -6,13 +6,13 @@ public class ReversingAstring {
 	public static void main(String[] args) {
 		String str = "abcde";
 		char[] ar = str.toCharArray();
-		
+
 		String new_str = "";
-		
-		for(int i = ar.length-1; i >=0; i--) {
+
+		for (int i = ar.length - 1; i >= 0; i--) {
 			new_str = new_str + ar[i];
 		}
-		
+
 		System.out.println(new_str);
 	}
 }
