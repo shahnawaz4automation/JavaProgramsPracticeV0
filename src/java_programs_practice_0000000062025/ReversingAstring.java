bfe7049419f4dbc41eda110c25077581297a3f28
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-26 22:24:55 +0530

    rqfwavds commit

---
+++ src/java_programs_practice_0000000062025/ReversingAstring.java
@@ -0,0 +1,18 @@
+package java_programs_practice_0000000062025;
+
+import java.util.Arrays;
+
+public class ReversingAstring {
+	public static void main(String[] args) {
+		String str = "abcde";
+		char[] ar = str.toCharArray();
+		
+		String new_str = "";
+		
+		for(int i = ar.length-1; i >=0; i--) {
+			new_str = new_str + ar[i];
+		}
+		
+		System.out.println(new_str);
+	}
+}
