35c40c631a83d589556ed9ae794be59c96948543
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-27 23:13:15 +0530

    98fjasd commit

--- src/java_programs_practice_0000000062025/ValidatingNumberToBePalindrome.java
+++ src/java_programs_practice_0000000062025/ValidatingNumberToBePalindrome.java
@@ -1,5 +1,9 @@
 package java_programs_practice_0000000062025;
 
 public class ValidatingNumberToBePalindrome {
-
+	public static void main(String[] args) {
+		int a = 1234;
+		
+		String num = String.valueOf(a);
+	}
 }
