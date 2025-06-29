302f36d24ee47980658565fbb4d4a0f37ab55a4d
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-29 18:04:05 +0530

    98sajf dcommit

---
+++ src/programs_practice_ABC062025/CountOfNumberOfDigits.java
@@ -0,0 +1,15 @@
+package programs_practice_ABC062025;
+
+public class CountOfNumberOfDigits {
+	public static void main(String[] args) {
+		int number = 1234;
+		
+		int count = 0;
+		
+		while(number>0) {
+			number = number/10;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
