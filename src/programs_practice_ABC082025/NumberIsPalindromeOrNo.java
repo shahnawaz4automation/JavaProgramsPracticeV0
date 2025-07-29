b67dfef588cfe6d14a8bc12f140ecfc9923c4a2d
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-29 21:44:13 +0530

    Checked if a number is palindrome or no.

---
+++ src/programs_practice_ABC082025/NumberIsPalindromeOrNo.java
@@ -0,0 +1,18 @@
+package programs_practice_ABC082025;
+
+public class NumberIsPalindromeOrNo {
+	
+	public static void main(String[] args) {
+		int a = 121;
+		String num = String.valueOf(a);
+		StringBuffer sb = new StringBuffer(num);
+		String num1 = String.valueOf(sb.reverse());
+		int b = Integer.parseInt(num1);
+		if(a==b) {
+			System.out.println("Number is a palindrome");
+		}
+		else {
+			System.out.println("Number is not palindrome");
+		}
+	}
+}
