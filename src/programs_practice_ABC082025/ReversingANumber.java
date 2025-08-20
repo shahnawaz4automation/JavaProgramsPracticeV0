9887057ccd33de74d82ccf6022e00f7b76f6b87c
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-08-20 21:58:06 +0530

    9jfasdfj update

--- src/programs_practice_ABC082025/ReversingANumber.java
+++ src/programs_practice_ABC082025/ReversingANumber.java
@@ -1,7 +1,9 @@
 package programs_practice_ABC082025;
 
-public class ReversingANumber {
-	public static void main(String[] args) {
+public class ReversingANumber 
+{
+	public static void main(String[] args) 
+	{
 		int a = 1234;
 		StringBuffer sb = new StringBuffer(String.valueOf(a));
 		System.out.println(sb.reverse());
