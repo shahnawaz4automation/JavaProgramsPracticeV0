db9f9330e04901ef0caeeb42ee38393a9cf10d0b
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-08-21 22:29:00 +0530

    asfdaef update

--- src/programs_practice_ABC082025/ReversingANumber.java
+++ src/programs_practice_ABC082025/ReversingANumber.java
@@ -4,6 +4,16 @@ public class ReversingANumber {
 	public static void main(String[] args) {
 		int a = 1234;
 		StringBuffer sb = new StringBuffer(String.valueOf(a));
-		System.out.println(sb.reverse());
+		StringBuffer sb1 = sb.reverse();
+		String a1 = sb1.toString();
+		
+		int b = Integer.parseInt(a1);
+		
+		if(a==b) {
+			System.out.println("a is palindrome");
+		}
+		else {
+			System.out.println("a is not palindrome");
+		}
 	}
 }
