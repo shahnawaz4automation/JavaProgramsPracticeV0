1bdd8a667a4f39c627721257f955b9726b64883f
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-08-29 20:45:00 +0530

    98fsajf update

--- src/programs_practice_ABC082025/ReversingAstring.java
+++ src/programs_practice_ABC082025/ReversingAstring.java
@@ -1,8 +1,7 @@
 package programs_practice_ABC082025;
 
 public class ReversingAstring {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) 	{
 		String s = "ABC";
 
 		char[] c = s.toCharArray();
