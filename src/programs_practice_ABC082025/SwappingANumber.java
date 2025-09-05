d6d0f9e9469e541b651d296c14bccc13061c3493
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-05 23:13:52 +0530

    jdsakfndsa update

---
+++ src/programs_practice_ABC082025/SwappingANumber.java
@@ -0,0 +1,12 @@
+package programs_practice_ABC082025;
+
+public class SwappingANumber {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+	int c = a;
+	a = b;
+	b= c;
+	System.out.println(a + "       " + b);
+}
+}
