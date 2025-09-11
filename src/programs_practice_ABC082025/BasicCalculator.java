d6ec3a2938426dcfe708aed4e0201bb6fc0ff7f7
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-11 22:53:21 +0530

    fjsaldf update

--- src/programs_practice_ABC082025/BasicCalculator.java
+++ src/programs_practice_ABC082025/BasicCalculator.java
@@ -7,22 +7,22 @@ public class BasicCalculator {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter first number: ");
 		double a = sc.nextDouble();
-		
-		System.out.print("Enter second number: ");
-        double b = sc.nextDouble();
-        
-        System.out.println("Sum: "+ (a+b));
-        System.out.println("Difference: "+ (a-b));
-        
-        System.out.println("Product: " + (a * b));
-        if (b != 0) {
-            System.out.println("Quotient: " + (a / b));
-            System.out.println("Remainder (a % b): " + (a % b));
-        } else {
-            System.out.println("Quotient: undefined (division by zero)");
-            System.out.println("Remainder: undefined (division by zero)");
-        }
 
-        sc.close();
+		System.out.print("Enter second number: ");
+		double b = sc.nextDouble();
+
+		System.out.println("Sum: " + (a + b));
+		System.out.println("Difference: " + (a - b));
+
+		System.out.println("Product: " + (a * b));
+		if (b != 0) {
+			System.out.println("Quotient: " + (a / b));
+			System.out.println("Remainder (a % b): " + (a % b));
+		} else {
+			System.out.println("Quotient: undefined (division by zero)");
+			System.out.println("Remainder: undefined (division by zero)");
+		}
+
+		sc.close();
 	}
 }
