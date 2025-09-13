e2f49eb65130a0b07140f2c99806faaf51dc0460
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-13 13:22:53 +0530

    usadfjsd commit

--- src/programs_practice_2025/SmallestOf3Numbers.java
+++ src/programs_practice_2025/SmallestOf3Numbers.java
@@ -1,5 +1,19 @@
 package programs_practice_2025;
 
 public class SmallestOf3Numbers {
-
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 3;
+		int c = 1;
+		
+		if(a==b || b==c || c==a) {
+			System.out.println("There are 2 same variables");
+		}else if (a <= b && a <= c) {
+			System.out.println("a is smallest");
+		} else if (b <= a && b <= c) {
+			System.out.println("b is smallest");
+		} else {
+			System.out.println("c is smallest");
+		}
+	}
 }
