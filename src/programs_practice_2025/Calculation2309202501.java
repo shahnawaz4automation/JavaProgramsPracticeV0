19f6dfb142578ee93766df1c95132d95ab2369be
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-23 20:35:39 +0530

    98sajd update

--- src/programs_practice_2025/Calculation2309202501.java
+++ src/programs_practice_2025/Calculation2309202501.java
@@ -3,5 +3,7 @@ package programs_practice_2025;
 public class Calculation2309202501 {
 	public static void main(String[] args) {
 		System.out.println(-6+2*4);
+		System.out.println((2+3)*4);
+		System.out.println(9+6-3*4/2%5); // * / % + -
 	}
 }
