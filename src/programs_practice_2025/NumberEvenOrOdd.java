c1531b273df7ca7258044d7627ddebce8f3eb330
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-24 19:20:26 +0530

    9sajfdsf update

---
+++ src/programs_practice_2025/NumberEvenOrOdd.java
@@ -0,0 +1,13 @@
+package programs_practice_2025;
+
+public class NumberEvenOrOdd {
+	public static void main(String[] args) {
+		int a = 10;
+
+		if (a % 2 == 0) {
+			System.out.println("Even number");
+		} else {
+			System.out.println("Odd number");
+		}
+	}
+}
