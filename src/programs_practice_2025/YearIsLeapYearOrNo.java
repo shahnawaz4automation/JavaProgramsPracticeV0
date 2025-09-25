918736a37db675687757f3481c4612b2d51384b1
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-09-25 23:12:24 +0530

    Leap year or no practice

---
+++ src/programs_practice_2025/YearIsLeapYearOrNo.java
@@ -0,0 +1,20 @@
+package programs_practice_2025;
+
+import java.util.Scanner;
+
+public class YearIsLeapYearOrNo {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int year = scan.nextInt();
+		
+		boolean status = false;
+		
+		if(year%4==0) {
+			if(year%100==0) {
+			else(year%400==0) {
+					status = 
+				}
+			}
+		}
+	}
+}
