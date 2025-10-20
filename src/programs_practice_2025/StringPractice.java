81fbb80e412dfb8eb5dbfc234c12ebdd6f231b6a
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-10-20 22:42:11 +0530

    98fadsjf update

---
+++ src/programs_practice_2025/StringPractice.java
@@ -0,0 +1,16 @@
+package programs_practice_2025;
+
+import java.lang.reflect.Array;
+
+public class StringPractice {
+	public static void main(String[] args) {
+		String s = "Sam";
+		System.out.println(s.length());
+		System.out.println(s.charAt(0));
+		System.out.println(s.charAt(2));
+		
+		int a[] = {1,2,3};
+ 		System.out.println(a.length);
+ 		for()
+	}
+}
---
+++ src/programs_practice_2025/SumOfFirst100Numbers.java
@@ -0,0 +1,15 @@
+package programs_practice_2025;
+
+public class SumOfFirst100Numbers {
+public static void main(String[] args) {
+	int sum = 0;
+	
+	for(int i = 1; i<=100; i++) {
+		sum = sum + i;
+	}
+	System.out.println(sum);
+	
+	double f = Math.PI;
+	System.out.println(f);
+}
+}
