6fb3ca5e6c378c6c7c0fc0e7062938cc376001fd
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-10-21 22:55:58 +0530

    89ru32jf update

--- src/programs_practice_2025/StringPractice.java
+++ src/programs_practice_2025/StringPractice.java
@@ -8,11 +8,11 @@ public class StringPractice {
 		System.out.println(s.length());
 		System.out.println(s.charAt(0));
 		System.out.println(s.charAt(2));
-		
-		int a[] = {1,2,3};
- 		System.out.println(a.length);
- 		for(int i=0;i<a.length;i++) {
- 			System.out.println(a[i]);
- 		}
+
+		int a[] = { 1, 2, 3 };
+		System.out.println(a.length);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
 	}
 }
