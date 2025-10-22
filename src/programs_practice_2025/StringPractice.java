c7d05a594be6939677ebaca86fcc03797e5a15c9
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-10-22 22:28:34 +0530

    04832f update

--- src/programs_practice_2025/StringPractice.java
+++ src/programs_practice_2025/StringPractice.java
@@ -4,15 +4,26 @@ import java.lang.reflect.Array;
 
 public class StringPractice {
 	public static void main(String[] args) {
+		
 		String s = "Sam";
 		System.out.println(s.length());
 		System.out.println(s.charAt(0));
 		System.out.println(s.charAt(2));
+		System.out.println(s+" Bahadur");
+		System.out.println(s);
+		
+		StringBuffer s1 = new StringBuffer("A");
+		System.out.println(s1+" Bahadur");
+		System.out.println(s1);
 
-		int a[] = { 1, 2, 3 };
-		System.out.println(a.length);
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
-		}
+		StringBuilder s2 = new StringBuilder("A");
+		System.out.println(s2+" Bahadur");
+		System.out.println(s2);
+		
+//		int a[] = { 1, 2, 3 };
+//		System.out.println(a.length);
+//		for (int i = 0; i < a.length; i++) {
+//			System.out.println(a[i]);
+//		}
 	}
 }
