1eb8a25bc3ff3bfd87f7952f633062b8fb027771
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-10-25 21:45:41 +0530

    f98dsajf update

---
+++ src/programs_practice_2025/StringImmutableTest.java
@@ -0,0 +1,14 @@
+package programs_practice_2025;
+
+public class StringImmutableTest {
+	public static void main(String[] args) {
+		String str1 = "Hello";    // A String object is created in String pool
+        String str2 = str1;       // str2 points to the same object as str1
+        //String str3 = new String("Java");
+        
+        //str1 = str1 + " World";
+        
+        System.out.println(str1.equals(str2));
+        System.out.println(str1==str2);
+	}
+}
