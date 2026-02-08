f23f282d682edb4e24c6df906c6dcab07f3bc270
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-02-08 22:27:07 +0530

    98ufjdsa update

--- src/programs_practice_2026/PracticeStrings.java
+++ src/programs_practice_2026/PracticeStrings.java
@@ -1,6 +1,13 @@
 package programs_practice_2026;
 
 public class PracticeStrings {
+	public static void main(String[] args) {
+		int age = 35;
+		double salary = 50000;
+		String emp_name = "Shahnawaz";
+		boolean isEmployed = true;
 
+		System.out.println("Name: " + emp_name);
 
+	}
 }
