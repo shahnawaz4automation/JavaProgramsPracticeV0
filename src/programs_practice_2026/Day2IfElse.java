c7e8b63cb67733f53312ef40902022baa867a466
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-02-09 21:07:24 +0530

    98fajsdf update

--- src/programs_practice_2026/Day2IfElse.java
+++ src/programs_practice_2026/Day2IfElse.java
@@ -3,11 +3,15 @@ package programs_practice_2026;
 public class Day2IfElse {
 	public static void main(String[] args) {
 		int marks = 65;
-		
-		if(marks>=60) {
-			System.out.println("Result: Passed");
-		}else {
-			System.out.println("Result: Failed");
+
+		if (marks >= 90) {
+			System.out.println("Grade: A");
+		} else {
+			if (marks >= 65) {
+				System.out.println("Grade: B");
+			} else {
+				System.out.println("Grade: C");
+			}
 		}
 	}
 }
