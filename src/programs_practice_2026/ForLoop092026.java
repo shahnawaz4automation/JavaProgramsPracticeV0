35645eb416af36ccff841623abbf40c89c7245d8
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-02-09 21:21:43 +0530

    09fadsjf update

---
+++ src/programs_practice_2026/ForLoop092026.java
@@ -0,0 +1,11 @@
+package programs_practice_2026;
+
+public class ForLoop092026 {
+	public static void main(String[] args) {
+		int number = 3;
+		
+		for(int i = 0; i<=number; i++) {
+			System.out.println("Iteration: "+ i);
+		}
+	}
+}
---
+++ src/programs_practice_2026/UserLoggedInOrNo.java
@@ -0,0 +1,13 @@
+package programs_practice_2026;
+
+public class UserLoggedInOrNo {
+	public static void main(String[] args) {
+		boolean isUserLoggedIn = false;
+		if(isUserLoggedIn){
+			System.out.println("User is logged in to the application");
+		}
+		else {
+			System.out.println("User is not logged in to the application");
+		}
+	}
+}
