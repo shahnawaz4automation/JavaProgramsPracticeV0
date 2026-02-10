13e13a0ac8f24d71ba0b94ed6926be529c383198
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-02-10 23:02:38 +0530

    98fjdsa update

---
+++ src/programs_practice_2026/Day3Methods.java
@@ -0,0 +1,11 @@
+package programs_practice_2026;
+
+public class Day3Methods {
+	public static void Hello() {
+		System.out.println("hello");
+	}
+public static void main(String[] args) {
+	Hello();
+	Hello();
+}
+}
