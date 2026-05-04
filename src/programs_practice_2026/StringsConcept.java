8bb9b113903a0d871e4eb02f552246b08c3f9c4a
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-05-04 20:13:57 +0530

    Update

--- src/programs_practice_2026/StringsConcept.java
+++ src/programs_practice_2026/StringsConcept.java
@@ -4,5 +4,14 @@ public class StringsConcept {
 	public static void main(String[] args) {
 		String a = "I am Mohammed Shahnawaz";
 		System.out.println("Length of the string is: "+ a.length());
+		
+		String b = "I am an Engineer";
+		System.out.println("Result of addition: "+a+b);
+		
+		System.out.println("Result of concatenation: "+ a.concat(b));
+		System.out.println("Result of concatenation: "+ b.concat(a));
+		
+		
+		
 	}
 }
