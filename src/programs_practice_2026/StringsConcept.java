545d8c8043433e4f43b5ca31a963c622897809b7
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-05-05 20:52:30 +0530

    Update

--- src/programs_practice_2026/StringsConcept.java
+++ src/programs_practice_2026/StringsConcept.java
@@ -2,6 +2,7 @@ package programs_practice_2026;
 
 public class StringsConcept {
 	public static void main(String[] args) {
+		
 		String a = "I am Mohammed Shahnawaz";
 		System.out.println("Length of the string is: "+ a.length());
 		
