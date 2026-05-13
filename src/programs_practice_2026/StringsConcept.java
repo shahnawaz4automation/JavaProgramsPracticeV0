0e5f5a34a5aa05e34d7770bb41fc39f225f7311b
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-05-13 22:33:30 +0530

    Update

--- src/programs_practice_2026/StringsConcept.java
+++ src/programs_practice_2026/StringsConcept.java
@@ -13,5 +13,7 @@ public class StringsConcept {
 		System.out.println("Result of concatenation: " + b.concat(a));
 
 		System.out.println("Result of trim: " + a.trim());
+		
+		System.out.println(a+b);
 	}
 }
