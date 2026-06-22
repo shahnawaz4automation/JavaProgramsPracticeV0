be1c37255910af02008dcbde0554d1ba729490a8
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-06-22 20:40:47 +0530

    Update

--- src/programs_practice_2026/A1MathOperation.java
+++ src/programs_practice_2026/A1MathOperation.java
@@ -3,7 +3,7 @@ package programs_practice_2026;
 public class A1MathOperation {
 
 	public static void main(String[] args) {
-		System.out.println((2+3)*4);
+		System.out.println(9+6-3*4/2%5);
 	}
 
 }
