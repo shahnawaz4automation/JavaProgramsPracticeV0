c9d14663c8456487693bf48d1934e4eb96b2a386
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-06-28 23:28:39 +0530

    About to start constructors

--- src/programs_practice_2026/A4OverloadingPractice.java
+++ src/programs_practice_2026/A4OverloadingPractice.java
@@ -1,15 +1,11 @@
 package programs_practice_2026;
 
 public class A4OverloadingPractice {
-	
-	void animal() {
-		System.out.println("Just a sound");
-	}
-	
-	void dog() extend
-
 	public static void main(String[] args) {
-		
+		System.out.println("About to start constructors");
+
 	}
+
+	
 
 }
