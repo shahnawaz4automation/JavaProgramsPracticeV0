b1354ba64f9bc5606cf8bcacf9e04fbe8fae9b3e
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-06-29 20:25:01 +0530

    Contructors

---
+++ src/programs_practice_2026/A4Constructors1.java
@@ -0,0 +1,12 @@
+package programs_practice_2026;
+
+public class A4Constructors1 {
+	String name;
+	A4Constructors1(String name){
+		this.name = name;
+	}
+	void display() {
+		System.out.println(this.name);
+		System.out.println(name);
+	}
+}
---
+++ src/programs_practice_2026/A4Constructors2.java
@@ -0,0 +1,8 @@
+package programs_practice_2026;
+
+public class A4Constructors2 {
+	public static void main(String[] args) {
+		A4Constructors1 a = new A4Constructors1("Shahnawaz");
+		a.display();
+	}	
+}
