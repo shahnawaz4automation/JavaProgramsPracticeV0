cb01ed43456b5518893546840c07468dc8e80a92
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-06-30 23:11:26 +0530

    Incorrect use of this keyword

--- src/programs_practice_2026/A4Constructors1.java
+++ src/programs_practice_2026/A4Constructors1.java
@@ -3,7 +3,7 @@ package programs_practice_2026;
 public class A4Constructors1 {
 	String name;
 	A4Constructors1(String name){
-		this.name = name;
+		this.name = this.name;
 	}
 	void display() {
 		System.out.println(this.name);
