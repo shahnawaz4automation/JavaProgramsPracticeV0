adfc38a864d61ff23a831b3d4880708a49519f26
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-07-04 17:03:22 +0530

    Methods()

---
+++ src/programs_practice_2025/Employee.java
@@ -0,0 +1,21 @@
+package programs_practice_2025;
+
+public class Employee {
+	String name;
+	String company;
+
+	Employee(String name, String company){
+		this.name = name;
+		this.company = company;
+	}
+	void introduce() {
+		System.out.println("Hi, my name is " + this.name);
+		System.out.println();
+		System.out.println("I work at "+this.company);
+	}
+	
+	public static void main(String[] args) {
+		Employee e = new Employee("Mohammed Shahnawaz", "KloudGin");
+		e.introduce();
+	}
+}
