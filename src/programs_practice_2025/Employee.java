5eb0cdda370e19c42feb153c2aba6cc99edd834b
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-07-05 23:21:06 +0530

    Update

--- src/programs_practice_2025/Employee.java
+++ src/programs_practice_2025/Employee.java
@@ -4,16 +4,17 @@ public class Employee {
 	String name;
 	String company;
 
-	Employee(String name, String company){
+	Employee(String name, String company) {
 		this.name = name;
 		this.company = company;
 	}
+
 	void introduce() {
 		System.out.println("Hi, my name is " + this.name);
 		System.out.println();
-		System.out.println("I work at "+this.company);
+		System.out.println("I work at " + this.company);
 	}
-	
+
 	public static void main(String[] args) {
 		Employee e = new Employee("Mohammed Shahnawaz", "KloudGin");
 		e.introduce();
