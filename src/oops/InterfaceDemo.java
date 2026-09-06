ef3b641f543984adef49c5e62725424bdb54bff0
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-09-06 13:01:53 +0530

    PrintWordsGreaterThan4

---
+++ src/oops/InterfaceDemo.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class InterfaceDemo {
+
+	interface Method{
+		void method1();
+		void method2();
+	}
+	class methodImplementation implements Method {
+
+		@Override
+		public void method1() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void method2() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+}
+}
---
+++ src/programs_practice_2026/PrintWordsGreaterThan4.java
@@ -0,0 +1,21 @@
+package programs_practice_2026;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintWordsGreaterThan4 {
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<>();
+		names.add("Rahul");
+		names.add("Amit");
+		names.add("John");
+		names.add("Sara");
+		names.add("Shahnawaz");
+
+		for (String name : names) {
+			if(name.length()>4) {
+				System.out.println(name);
+			}
+		}
+	}
+}
