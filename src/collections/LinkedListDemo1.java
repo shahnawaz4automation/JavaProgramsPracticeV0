24a92d094722af8444ce867bf5a10f7d394f636e
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-09-20 20:27:42 +0530

    Linkedlist practice

---
+++ src/collections/LinkedListDemo1.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo1 {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(100);
+		list.add("welcome");
+		list.add(15.5);
+		list.add('A');
+		list.add(true);
+		list.add(null);
+		
+		System.out.println(list);
+		
+		System.out.println(list.size());
+		
+		//remove
+		list.remove(3);
+		System.out.println("After removing, new list: "+list);
+		
+		//Insert add element in the middle of linked list
+		list.add(3,"Java");
+		System.out.println("After adding an element in the middle, new list: "+list);
+		}
+}
