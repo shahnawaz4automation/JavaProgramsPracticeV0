64a78652cfcc7e410e05dd49cdcde1ddcf412e78
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-09-20 20:13:10 +0530

    Update

--- src/oops/InterfaceDemo.java
+++ src/oops/InterfaceDemo.java
@@ -2,24 +2,24 @@ package oops;
 
 public class InterfaceDemo {
 
-	interface Method{
+	interface Method {
 		void method1();
+
 		void method2();
 	}
+
 	class methodImplementation implements Method {
 
 		@Override
 		public void method1() {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void method2() {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
 	}
-}
 }
