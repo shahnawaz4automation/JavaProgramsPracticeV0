334c62ddbdaa119989d369a00bc11cc50d936f99
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-09-22 21:27:55 +0530

    Updated

--- src/collections/LinkedListDemo1.java
+++ src/collections/LinkedListDemo1.java
@@ -11,17 +11,17 @@ public class LinkedListDemo1 {
 		list.add('A');
 		list.add(true);
 		list.add(null);
-		
+
 		System.out.println(list);
-		
+
 		System.out.println(list.size());
-		
-		//remove
+
+		// remove
 		list.remove(3);
-		System.out.println("After removing, new list: "+list);
-		
-		//Insert add element in the middle of linked list
-		list.add(3,"Java");
-		System.out.println("After adding an element in the middle, new list: "+list);
-		}
+		System.out.println("After removing, new list: " + list);
+
+		// Insert add element in the middle of linked list
+		list.add(3, "Java");
+		System.out.println("After adding an element in the middle, new list: " + list);
+	}
 }
